ba3b2933b1958f421f143ac92265c6a05bbd5cd5
Matěj K <hobitekCZ@post.cz>
2023-08-13 10:10:37 +0200

    Header with custom font

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+	/** @type {import('./$types').LayoutData} */
+	export let data;
+</script>
+
+<section><a href="/">Tomáto Půjčovna</a></section>
+<slot />
+
+<style>
+	section {
+		width: 100vw;
+		height: 3.75rem;
+		background: #222;
+	}
+	section a {
+		text-decoration: none;
+		color: #fff;
+		font-family: Leckerli One;
+		font-size: 2.5rem;
+		font-style: normal;
+		font-weight: 400;
+		line-height: normal;
+	}
+</style>
